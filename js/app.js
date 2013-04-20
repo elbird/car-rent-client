027a33cf12f09782f55aaafc9bf6608f414bd5f7
Sebastian Vogel <s.vogel@gentics.com>
2013-04-21 01:29:40 +0200

    client v0.1

--- js/app.js
+++ js/app.js
@@ -2,15 +2,15 @@
 
 /* App Module */
 
-angular.module('carRental', ['adminServices', 'ui.bootstrap']).
+angular.module('carRental', ['adminServices', 'userServices', 'ui.bootstrap', 'ngCookies']).
 config(['$routeProvider', function($routeProvider) {
 	$routeProvider
 	.when('/home', {templateUrl: 'partials/home.html', controller: HomeCtrl})
-	.when('/rent/', {templateUrl: 'partials/rent/rent-locations.html', controller: RentLocationsCtrl})
-	.when('/rent/:locationId', {templateUrl: 'partials/rent/rent-location-cars.html', controller: RentLocationsCarsCtrl})
+	.when('/rent/', {templateUrl: 'partials/locations.html', controller: RentLocationsCtrl})
+	.when('/rent/:locationId', {templateUrl: 'partials/location-cars.html', controller: RentLocationsCarsCtrl})
 	.when('/rent/:locationId/car/:carId/book', {templateUrl: 'partials/rent/booking.html', controller: RentBookingCtrl})
-	.when('/admin/', {templateUrl: 'partials/admin/location-list.html', controller: LocationListCtrl})
-	.when('/admin/:locationId', {templateUrl: 'partials/admin/location-cars.html', controller: AdminLocationDetailCtrl})
+	.when('/admin/', {templateUrl: 'partials/locations.html', controller: LocationListCtrl})
+	.when('/admin/:locationId', {templateUrl: 'partials/location-cars.html', controller: AdminLocationDetailCtrl})
 	.when('/admin/:locationId/car/new', {templateUrl: 'partials/admin/car-edit.html', controller: AdminEditCarCtrl})
 	.when('/admin/:locationId/car/:carId', {templateUrl: 'partials/admin/car-edit.html', controller: AdminEditCarCtrl})
 	.otherwise({redirectTo: '/home'});
--- js/controllers.js
+++ js/controllers.js
@@ -1,7 +1,10 @@
 'use strict';
 
+
 /* Controllers */
-function MainCtrl($scope, $location) {
+function MainCtrl($scope, $location, $cookieStore, User) {
+	$scope.loginInfo = User.getLoginInfo();
+
 	$scope.navItems = [{
 		id : "home",
 		name: "Home",
@@ -18,6 +21,14 @@ function MainCtrl($scope, $location) {
 		url: "#/admin/",
 		active: ""
 	}];
+
+	$scope.login = function () {
+		$scope.loginInfo = User.login(this.loginEmail, this.loginPassword);
+	};
+
+	$scope.logout = function () {
+		$scope.loginInfo = User.logout();
+	};
 
 	function setRessources(path) {
 		var i,
@@ -50,18 +61,49 @@ function MainCtrl($scope, $location) {
 
 
 
-function LocationListCtrl($scope, LocationCar) {
-  $scope.locations = LocationCar.Location.query();
-  $scope.orderProp = 'zip';
-}
-
 
 /**
  * HOME Section
  */
 
-function HomeCtrl($scope, $location, LocationCar) {
-
+function HomeCtrl($scope, $location, $q, $dialog, User, Booking, LocationCar, CalculatePrice) {
+	$scope.loginInfo = User.getLoginInfo();
+	$scope.bookings = Booking.query({user_id: $scope.loginInfo.user.id});
+	$scope.cars = {};
+	$scope.bookings.$then(function () {
+		var i, carId, locationId,
+			cars = [];
+		for (i = 0; i < $scope.bookings.length; i++) {
+			if ($scope.bookings[i].rentalcar_id) {
+				carId = $scope.bookings[i].rentalcar_id.split('/')[2];
+				locationId = $scope.bookings[i].rentalcar_id.split('/')[1];
+				cars.push(LocationCar.Car.get({rentallocation_id: locationId, id: carId}));
+				$scope.bookings[i].price = CalculatePrice.calculate({id: $scope.bookings[i].id});
+			}
+		}
+		return $q.all(cars);
+	}).then(function (cars) {
+		var i, carsObj = {};
+		for (i = 0; i < cars.length; i++) {
+			cars[i].$then(function (car) {
+				$scope.cars[car.data.id] = car.data;
+			});
+		}
+	});
+	$scope.deleteBooking = function (bookinId) {
+  	var title = 'Buchung löschen',
+  		msg = 'Wollen Sie dieseBuchung wirklich löschen? Diese Aktion kann nicht rückgängig gemacht werden!',
+  		btns = [{result:'cancel', label: 'Abbrechen'}, {result:'ok', label: 'Löschen', cssClass: 'btn-danger'}];
+  	$dialog.messageBox(title, msg, btns)
+	    .open()
+	    .then(function(result){
+	    	if (result === "ok") {
+	    		Booking.remove({user_id: $scope.loginInfo.user.id, id: bookinId.split('/')[2]}).$then(function () {
+		  			$scope.bookings = Booking.query({user_id: $scope.loginInfo.user.id});
+		  		});
+	    	}
+	  	});
+  };
 }
 
 
@@ -69,23 +111,79 @@ function HomeCtrl($scope, $location, LocationCar) {
  * RENT Section
  */
 
-function RentLocationsCtrl($scope, $location, LocationCar) {
-
+function RentLocationsCtrl($scope, $location, LocationCar, User) {
+	$scope.module = "rent";
+	$scope.loginInfo = User.getLoginInfo(true);
+	$scope.locations = LocationCar.Location.query();
+  	$scope.orderProp = 'zip';
 }
 
-function RentLocationsCarsCtrl($scope, $location, LocationCar) {
-
+function RentLocationsCarsCtrl($scope, $location, $routeParams, User, LocationCar) {
+	$scope.module = "rent";
+	$scope.loginInfo = User.getLoginInfo(true);
+	$scope.location = LocationCar.Location.get({locationId: $routeParams.locationId});
+  	$scope.cars = LocationCar.Car.query({rentallocation_id: $routeParams.locationId});
 }
 
-function RentBookingCtrl($scope, $location, LocationCar) {
-
+function RentBookingCtrl($scope, $routeParams, $location, Booking, User, LocationCar, CalculatePrice) {
+	$scope.module = "rent";
+	$scope.loginInfo = User.getLoginInfo(true);
+	$scope.car = LocationCar.Car.get({rentallocation_id: $routeParams.locationId, id: $routeParams.carId});
+	$scope.location = LocationCar.Location.get({locationId: $routeParams.locationId});
+	$scope.price = {result: ""};
+	if ($routeParams.bookingId) {
+		$scope.booking = Booking.get({user_id: $scope.loginInfo.user.id, id: $routeParams.bookingId});
+	} else {
+		$scope.booking = new Booking({user_id: $scope.loginInfo.user.id});
+	}
+	$scope.submit = function () {
+		function saveBooking() {
+			if ($routeParams.bookingId) {
+				$scope.car.angularShortId = $routeParams.bookingId;
+			}
+			$scope.booking.user_id = $scope.loginInfo.user.id;
+			$scope.booking.rentalcar_id = $scope.car.id;
+			$scope.booking.$save(function () {
+				$location.path('/home');
+			}, function (err) {
+				console.log(err);
+				$scope.formError = true;
+			});
+		}
+		if ($scope.booking.$then) {
+			$scope.booking.$then(saveBooking);
+		} else {
+			saveBooking();
+		}
+	};
+	$scope.cancel = function () {
+		$location.path('/rent/' + $routeParams.locationId);
+	};
+	$scope.calculatePrice = function (days, currency) {
+		if (!$scope.booking.days || !$scope.booking.currency || $scope.booking.currency.length !== 3) {
+			return;
+		}
+		$scope.price = CalculatePrice.calculate({
+			rentalcar_id: $scope.car.id,
+			days: $scope.booking.days,
+			currency: $scope.booking.currency
+		});
+	};
 }
 
 /**
  * ADMIN Section
  */
 
+function LocationListCtrl($scope, LocationCar) {
+	$scope.module = "admin";
+  	$scope.locations = LocationCar.Location.query();
+  	$scope.orderProp = 'zip';
+}
+
+
 function AdminLocationDetailCtrl($scope, $routeParams, $dialog, $location, LocationCar) {
+  $scope.module = "admin";
   $scope.location = LocationCar.Location.get({locationId: $routeParams.locationId});
   $scope.cars = LocationCar.Car.query({rentallocation_id: $routeParams.locationId});
   $scope.newCar = function () {
@@ -105,10 +203,6 @@ function AdminLocationDetailCtrl($scope, $routeParams, $dialog, $location, Locat
 	    	}
 	  	});
   };
-/*
-  $scope.setImage = function(imageUrl) {
-    $scope.mainImageUrl = imageUrl;
-  }*/
 }
 
 function AdminEditCarCtrl($scope, $routeParams, $location, LocationCar) {
